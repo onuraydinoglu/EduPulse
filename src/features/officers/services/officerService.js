9a3f9c70339b271c0522ad2361bf9396a887d3cc
onuraydinoglu <onur_ist_98@hotmail.com>
2026-04-29 12:06:09 +0300

    user silme işlemleri güncellendi.

--- src/features/officers/services/officerService.js
+++ src/features/officers/services/officerService.js
@@ -16,13 +16,13 @@ export const officerService = {
   },
 
   update: async (data) => {
-    const response = await axiosInstance.put(API_ENDPOINTS.USERS.BASE, data);
+    const response = await axiosInstance.put(API_ENDPOINTS.USERS.OFFICER, data);
     return response.data;
   },
 
   delete: async (id) => {
     const response = await axiosInstance.delete(
-      `${API_ENDPOINTS.USERS.BASE}/${id}`,
+      `${API_ENDPOINTS.USERS.OFFICER}/${id}`,
     );
     return response.data;
   },
